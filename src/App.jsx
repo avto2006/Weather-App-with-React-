0f0142390c53930390b00a6e5f7c287268b87a00
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-01-21 19:23:13 +0400

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './index.css';
 
+
 function App() {
 
 
   return (
-    <>
-     
-    </>
+    <div className='full'>
+      <div className="text_area">
+        <h1>Weather App</h1>
+      </div>
+
+      <div className="input_area">
+        <input className='search' type="search" />
+      </div>
+    </div>
   )
 }
 
